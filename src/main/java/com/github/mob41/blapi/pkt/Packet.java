8b8415f30ee073e6adf7088f00eb832796b520e7
mob41 <puihuenlaw@gmail.com>
2017-02-06 18:43:11 +0800

    Partial: Comm packets: Packet Interface

---
+++ src/main/java/com/github/mob41/blapi/pkt/Packet.java
@@ -0,0 +1,18 @@
+package com.github.mob41.blapi.pkt;
+
+/**
+ * Packet interface<br>
+ * <br>
+ * This is probably useless...
+ * @author Anthony
+ *
+ */
+public interface Packet {
+
+	/**
+	 * Returns this packet's final compiled data
+	 * @return
+	 */
+	public byte[] getData();
+	
+}
