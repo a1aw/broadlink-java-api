58a00ad1b082d14c58d031484b937c9b76ec25d3
Christian Fischer <sw-dev@computerlyrik.de>
2017-04-13 23:40:33 +0200

    update/add copyright header

--- src/main/java/com/github/mob41/blapi/BLDevice.java
+++ src/main/java/com/github/mob41/blapi/BLDevice.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * MIT License
  *
- * Copyright (c) 2017 Anthony Law
+ * Copyright (c) 2017 Anthony Law, bwssystems, Christian Fischer (computerlyrik)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
