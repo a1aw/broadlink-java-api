10d553148a97087bb1609ac5dc9577656f9c0e35
mob41 <puihuenlaw@gmail.com>
2017-04-21 17:27:26 +0800

    Fixes copyright

--- src/main/java/com/github/mob41/blapi/pkt/BytePayload.java
+++ src/main/java/com/github/mob41/blapi/pkt/BytePayload.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * MIT License
  *
- * Copyright (c) 2017 Anthony Law
+ * Copyright (c) 2016, 2017 Anthony Law
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,11 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
+ * Contributors:
+ *     - Anthony Law (mob41) - Initial API Implementation
+ *     - bwssytems
+ *     - Christian Fischer (computerlyrik)
  *******************************************************************************/
 package com.github.mob41.blapi.pkt;
 
